59d1eb1ddae66edbcb0b0feb34a78ec622c204f3
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-05-01 15:33:38 +0500

    Add VaccineAdapter.java and list_item.xml

---
+++ app/src/main/java/com/amoueed/pakistanvaccineschedule/adapter/VaccineAdapter.java
@@ -0,0 +1,79 @@
+package com.amoueed.pakistanvaccineschedule.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.amoueed.pakistanvaccineschedule.R;
+import com.amoueed.pakistanvaccineschedule.model.Vaccine;
+
+import java.util.ArrayList;
+
+public class VaccineAdapter extends RecyclerView.Adapter<VaccineAdapter.ViewHolder>{
+
+    private ArrayList<Vaccine> mVaccineData;
+    private Context mContext;
+
+    VaccineAdapter(Context context, ArrayList<Vaccine> vaccineData) {
+        this.mVaccineData = vaccineData;
+        this.mContext = context;
+    }
+
+    @Override
+    public VaccineAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext).
+                inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(VaccineAdapter.ViewHolder holder, int position) {
+        // Get current vaccine.
+        Vaccine currentVaccine = mVaccineData.get(position);
+
+        // Populate the textviews with data.
+        holder.bindTo(currentVaccine);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mVaccineData.size();
+    }
+
+    /**
+     * ViewHolder class that represents each row of data in the RecyclerView.
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Member Variables for the TextViews
+        private TextView mVaccineNameText;
+        private TextView mDueDateText;
+        private TextView mGivenDateText;
+        private TextView mVaccineStatusText;
+        private TextView mReminderStatusText;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            // Initialize the views.
+            mVaccineNameText = itemView.findViewById(R.id.vaccine_name);
+            mDueDateText = itemView.findViewById(R.id.due_date);
+            mGivenDateText = itemView.findViewById(R.id.given_date);
+            mVaccineStatusText = itemView.findViewById(R.id.vaccine_status);
+            mReminderStatusText = itemView.findViewById(R.id.vaccine_reminder_status);
+        }
+
+        void bindTo(Vaccine currentVaccine){
+            // Populate the textviews with data.
+            mVaccineNameText.setText(currentVaccine.getName());
+            mDueDateText.setText(currentVaccine.getDueDate());
+            mGivenDateText.setText(currentVaccine.getGivenDate());
+            mVaccineStatusText.setText(currentVaccine.getStatus());
+            mReminderStatusText.setText(currentVaccine.getReminderStatus());
+
+        }
+    }
+
+}
